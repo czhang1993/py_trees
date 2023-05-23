0a051bebf22a84fff9ccabdaa2b59bcd470514eb
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-23 10:28:18 +1000

    Update tree.py

--- tree.py
+++ tree.py
@@ -8,3 +8,28 @@ class Node:
         self.n_node_samples = n_node_samples
         self.weighted_n_node_samples = weighted_n_node_samples
         self.missing_go_to_left = missing_go_to_left
+
+        
+class Tree:
+    __init__(self, n_features, n_classes, n_outputs, max_n_classes, max_depth, node_count, capacity, nodes, value, value_stride):
+        # input and output layout
+        self.n_features = n_features
+        self.n_classes = n_classes
+        self.n_outputs = n_outputs
+        self.max_n_classes = max_n_classes
+        
+        # inner structure
+        self.max_depth = max_depth
+        self.node_count = node_count
+        self.capacity = capacity
+        self.nodes = nodes
+        self.value = value
+        self.value_stride = value_stride
+        
+        
+class TreeBuilder:
+    __init__(self, tree, x, y, sample_weight=None, feature_has_missing=None):
+        pass
+    
+    
+    
