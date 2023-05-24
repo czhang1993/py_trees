bf0332847a6c178cd158029d76503d6b3938322d
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-24 16:36:04 +1000

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -1,7 +1,54 @@
 class Splitter:
     def __init__(self, criterion, max_features, min_samples_leaf, min_weight_leaf, random_state):
         self.criterion = criterion
+        
+        self.n_samples = 0
+        self.n_features = 0
+        
         self.max_features = max_features
         self.min_samples_leaf = min_samples_leaf
         self.min_weight_leaf = min_weight_leaf
         self.random_state = random_state
+        
+    def init(self, x, y, sample_weight):
+        self.rand_r_state = self.random_state.randint(0, RAND_R_MAX)
+        
+        n_samples = x.shape[0]
+        
+        self.samples = np.empty(n_samples)
+        
+        samples = self.samples
+        weighted_n_samples = 0.0
+        j = 0
+        
+        for i in range(n_samples):
+            # only work with positively weighted samples
+            if sample_weight is None or sample_weight[i] != 0.0:
+                samples[j] = i
+                j += 1
+
+            if sample_weight is not None:
+                weighted_n_samples += sample_weight[i]
+            else:
+                weighted_n_samples += 1.0
+        
+        # number of samples is the number of positively weighted samples
+        self.n_samples = j
+        self.weighted_n_samples = weighted_n_samples
+
+        n_features = x.shape[1]
+        self.features = np.arange(n_features)
+        self.n_features = n_features
+
+        self.feature_values = np.empty(n_samples)
+        self.constant_features = np.empty(n_features)
+
+        self.y = y
+
+        self.sample_weight = sample_weight
+        if feature_has_missing is not None:
+            self.criterion.init_sum_missing()
+        return 0
+        
+        
+        
